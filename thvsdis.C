80b0ffe48579c4c164cc6a40304dafbd2df3e3f9
Simon-Huiberts <47213257+Simon-Huiberts@users.noreply.github.com>
2019-02-20 12:59:09 +0100

    Update thvsdis.C

--- thvsdis.C
+++ thvsdis.C
@@ -1,27 +1,21 @@
-//Plotting the Threshold vs. mean
-
-
+//Plotting Threshold vs. Global DAC units
 {
 
    TCanvas *c1 = new TCanvas("c1","Untuned threshold vs Dispersion",200,40,700,500);
    c1->SetGrid();
 
-   // create the multigraph
+   // Creating multigraph
    TMultiGraph *mg = new TMultiGraph();
    mg->SetTitle("Untuned Threshold Dispersion vs. VTH1");
 
-   // create the graphs points
+   //Create the points and error
    const Int_t n = 8;
    Double_t x1[] = {200,300,400,500,600,650,700,750};
    Double_t dx1[] = {0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1};
    Double_t y1[] = {359.5, 341, 357.6, 400, 511, 637.9, 1035, 1223};
    Double_t dy1[] = {4.1, 4.2, 4.6,5.6,7.6,11.5,16,21.8};
 
-
-
-
-
-   // create the 1st TGraph
+   //Adding the points to a graph
    gr1 = new TGraphErrors(n,x1,y1,dx1,dy1);
    gr1->SetMarkerStyle(21);
    gr1->SetMarkerColor(3);
@@ -29,10 +23,10 @@
    gr1->SetLineWidth(2);
    gr1->SetFillStyle(0);
 
-   // put the graphs in the multigraph
+   //Putting the graph into multigraph
    mg->Add(gr1);
 
-   // draw the multigraph
+   // Draw the multigraph and set outlook
    mg->Draw("AP");
    mg->GetXaxis()->SetTitle("VTH1 [DAC]");
    mg->GetXaxis()->SetLabelSize(0.05);
@@ -47,15 +41,12 @@
    mg->GetYaxis()->SetTitleOffset(1);
    mg->GetYaxis()->SetLabelFont(42);
    mg->GetYaxis()->CenterTitle();
-   //mg->Fit("pol1","FQ");
    gStyle->SetOptFit(0001);
 
-   //c1->BuildLegend();
+   //Creating the fit function
    f = new TF1("f","[0] + [1]*x^3 - [2]*x^2 ",200,700);
    f->SetParNames("quad","u");
-
    mg->Fit(f);
 
-   //c1->BuildLegend();
    return c1;
 }
